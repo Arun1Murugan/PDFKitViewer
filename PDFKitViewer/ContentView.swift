0565519dcb4b291676b294acc5a2baffc945cdf2
Arun <arun14072001@gmail.com>
2025-04-15 20:40:31 +0530

    Initial Commit

---
+++ PDFKitViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PDFKitViewer
+//
+//  Created by Arun M on 15/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PDFKitViewer/PDFKitViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFKitViewerApp.swift
+//  PDFKitViewer
+//
+//  Created by Arun M on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct PDFKitViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
